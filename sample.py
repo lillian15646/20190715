c5082809d4991db64b488e4e31ba3c1b5ab23279
lillian15646 <52898988+lillian15646@users.noreply.github.com>
2019-07-15 11:17:54 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -17,8 +17,6 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
-message=StickerSendMesssage( package_id='1',sticker_id='1')
-line_bot_api.reply_message(event.reply_token,message)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -38,13 +36,15 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 回應使用者輸入的話
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+    message=StickerSendMesssage( package_id='1',sticker_id='1')
+    line_bot_api.reply_message(event.reply_token,message)
+
 
 
 if __name__ == "__main__":
