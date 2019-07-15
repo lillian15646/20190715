91f1a26d662240b496507cba96454a3101ef8f27
lillian15646 <52898988+lillian15646@users.noreply.github.com>
2019-07-15 12:15:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,16 +21,16 @@ app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-# line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
-# handler = WebhookHandler(config['line_bot']['Channel_Secret'])
-# client_id = config['imgur_api']['Client_ID']
-# client_secret = config['imgur_api']['Client_Secret']
-# album_id = config['imgur_api']['Album_ID']
-# API_Get_Image = config['other_api']['API_Get_Image']
+line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
+handler = WebhookHandler(config['line_bot']['Channel_Secret'])
+client_id = config['imgur_api']['Client_ID']
+client_secret = config['imgur_api']['Client_Secret']
+album_id = config['imgur_api']['Album_ID']
+API_Get_Image = config['other_api']['API_Get_Image']
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
-google_api_key = os.environ['GOOGLE_API_KEY']
+#google_api_key = os.environ['GOOGLE_API_KEY']
 line_reply_api = 'https://api.line.me/v2/bot/message/reply'
 
 
